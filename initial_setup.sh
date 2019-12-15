d365ae221cd4f4817bd3f9f8d9f0b92c91eb589a
data-modeler <26942296+data-modeler@users.noreply.github.com>
2019-12-15 08:18:07 -0500

    Add a file to set up the linux machine

---
+++ initial_setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install vim tmux python3 python3-pip -y
+pip install virtualenvwrapper
+
+cp .bash_aliases ~/.bash_aliases
+cp .bash_prompt ~/.bash_prompt
+cp .envrc ~/.envrc
+cp .tmux.conf ~/.tmux.conf
+cp .vimrc ~/.vimrc
+
+source ~/.bash_aliases
