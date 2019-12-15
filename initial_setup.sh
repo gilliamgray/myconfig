cd805d0ac584f02f2b0497df16192ef119f82d7f
data-modeler <26942296+data-modeler@users.noreply.github.com>
2019-12-15 13:13:17 -0500

    Add vim customizations to setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,17 @@
 sudo apt-get install vim tmux python3 python3-pip -y
 pip3 install virtualenvwrapper
 
+## set up vim bundles
+# vim pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# vim solarized
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
+
+cd ~
 cp .bash_aliases ~/.bash_aliases
 cp .bash_prompt ~/.bash_prompt
 cp .envrc ~/.envrc
